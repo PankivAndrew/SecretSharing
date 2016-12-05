275a448342f3ca73d4f81f4e21b2bd96784f23d6
arsen <arssenk@gmail.com>
2016-12-05 18:55:45 +0200

    l

--- Secret_Sharing.py
+++ Secret_Sharing.py
@@ -1,36 +1,37 @@
 import random
+
 import work_with_matrix
 
-class Secret:
 
+class Secret:
     def __init__(self, secret):
         self.secret = secret
 
     def choose_secret_vector(self):
         pass
-    def genetare_vect_a(self, number, size):
-        vectors = []
-        while not self.check_independency(vectors):
-            vectors = []
-            for i in range(number):
 
-                vectors.append([])
+    def genetare_vect_a(self, number, size):
+        # while not self.check_independency(vectors):
+        vectors = [[[], [],1], [[], [],2], [[], [],3], [[], [],4], [[], [],5]]
+        for i in range(number // 2):
+            for g in range(2):
                 for j in range(size):
-                  vectors[i].append(random.randint(0,1))
+                    vectors[i][g].append(random.randint(0, 1))
+        self.check_independency(vectors)
         return vectors
+
     def check_independency(self, matrix):
-        if matrix != []:
-            for i in range(len(matrix) - 2):
+        if matrix:
+            for i in range(1, len(matrix) - 2):
                 for j in range(i + 1, len(matrix) - 1):
                     for k in range(j + 1, len(matrix)):
 
                         a = [matrix[i], matrix[j], matrix[k]]
-                        if not work_with_matrix.check_matrix(a):
-                            print('qwerftgyhujikljhgfghjgf')
-                            return False
-            return True
+                        print(a)
+
         else:
             return False
 
+
 a = Secret(1)
-print(a.genetare_vect_a(5,3))
+a.genetare_vect_a(10, 6)
--- sx.py
+++ sx.py
@@ -1,8 +1,11 @@
-def perebir(lst):
-    a = []
-    for i in range(len(lst)- 2):
-        for j in range(i+1, len(lst) -1):
-            for k in range(j+1, len(lst)):
-                a.append([lst[i], lst[j], lst[k]])
-    print(a)
-perebir([1,2,3,4,5])
+# def perebir(lst):
+#     a = []
+#     for i in range(len(lst)- 2):
+#         for j in range(i+1, len(lst) -1):
+#             for k in range(j+1, len(lst)):
+#                 a.append([lst[i], lst[j], lst[k]])
+#     print(a)
+# perebir([1,2,3,4,5])
+a = set()
+a.add(4)
+print(2%2)
